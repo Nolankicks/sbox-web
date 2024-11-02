e48bfc08abe7a13c08d655f263c78d82e6825cc5
Kicks <nolandimensions@gmail.com>
2024-11-02 19:31:01 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import autoImport from 'sveltekit-autoimport';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
-
     kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             pages: 'build',
             assets: 'build',
@@ -20,22 +12,21 @@ const config = {
             fallback: '404.html',
         }),
         paths: {
-			base: process.argv.includes('dev') ? '' : '/sveltekit-autoimport'
-		},
+            base: process.env.NODE_ENV === 'development' ? '' : '/sveltekit-autoimport'
+        },
         prerender: {
             handleMissingId: ({ event, resolve }) => {
                 return;
             }
         }
     },
-
     extensions: ['.svelte', '.md', '.svx'],
     preprocess: [
         vitePreprocess(),
         autoImport({
-			include: ['**/*.(svelte)'],
-			components: ['./src/lib/components/', { name: './src' }]
-		})
+            include: ['**/*.(svelte)'],
+            components: ['./src/lib/components/', { name: './src' }]
+        })
     ],
 };
 
