7cf2b6fda6b65a1403bbc621001def2198b9e4ef
Kicks <nolandimensions@gmail.com>
2024-11-02 19:27:40 -0400

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,11 @@ const config = {
             assets: 'build',
             precompress: false,
             strict: true,
+            fallback: '404.html',
         }),
+        paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
         prerender: {
             handleMissingId: ({ event, resolve }) => {
                 return;
