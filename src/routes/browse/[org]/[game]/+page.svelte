644d711addf91c7af857dcca88a97b2cd4ea7c96
Kicks <nolandimensions@gmail.com>
2024-11-02 23:54:46 -0400

    Update +page.svelte

--- src/routes/browse/[org]/[game]/+page.svelte
+++ src/routes/browse/[org]/[game]/+page.svelte
@@ -9,13 +9,13 @@
 
 <div class="flex flex-col mt-5 px-5 items-center">
     {#if data.pkg}
-        <div class="flex items-center gap-6 bg-alt p-4 rounded-md">
+        <div class="flex items-center gap-6 bg-alt p-2 rounded-md">
+            <img src={data.pkg.Thumb} alt={data.pkg.Title} class="h-32 w-32 rounded-md" />
             <div class="flex flex-col">
                 <div class="text-3xl font-bold">{data.pkg.Title}</div>
                 <div class="text-xl">From <a class="underline" href="{base}/browse/{data.pkg?.Org.Ident}">{data.pkg?.Org.Title}</a></div>
-                <div class="text-xl">Version {data.pkg.Summary}</div>
+                <div class="text-xl">{data.pkg.Summary}</div>
             </div>
-            <img src={data.pkg.Thumb} alt={data.pkg.Title} class="h-32 w-32 rounded-md" />
         </div>
 
         <DiscordEmbed title={data.pkg.Title} description="from {data.pkg.Org.Title}" image={data.pkg.Thumb} />
